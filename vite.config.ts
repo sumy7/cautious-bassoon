1b83439d1bf1f7ea82ce8dfb8f8cf20c5b1afec2
sumy <sumyggsun@gmail.com>
2025-11-21 18:08:04 +0000

    chore: add vite and vitest

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+    port: 3000,
+  },
+  build: {
+    outDir: 'build',
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    css: true,
+  },
+});
